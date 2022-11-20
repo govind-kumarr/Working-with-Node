8e08d1c798217933d56f91eed497ce5ae9f5ffa4
govind kumar <gk4051668@gmail.com>
2022-11-20 19:11:48 +0530

    added assignment and its solution

--- app.js
+++ app.js
@@ -1,33 +1,7 @@
 const http = require("http");
 
-const fs = require("fs");
+const routes = require("./routes.js");
 
-const server = http.createServer((req, res) => {
-  const { url, method } = req;
-
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write(
-      "<body><form action='/message' method='POST' > <input type='text' name='message'/> <button type='submit' >submit</button></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-
-  if ((url === "/message" && method === "POST")) {
-    fs.writeFileSync("message.txt", "DUMMY");
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
-
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
