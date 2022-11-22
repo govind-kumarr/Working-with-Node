8655c84c62e8a408a93298e13a3c22e52b5452b1
govind kumar <gk4051668@gmail.com>
2022-11-22 17:34:33 +0530

    added templating engines

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require("path");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "views");
+
 const adminRoutes = require("./routes/admin.js");
 const shopRoutes = require("./routes/shop.js");
 
